58c03f06f5131d2d70268c5ca65176d691e06d9c
Gürkan <gurkan.andac@hva.nl>
2025-09-28 02:51:40 +0200

    updated gitignore

---
+++ infrastructure/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "germanywestcentral"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = "knmi-rg"
+  description = "Name of the Resource Group"
+}
