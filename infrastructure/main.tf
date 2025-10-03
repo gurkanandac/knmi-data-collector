746615f8edd349524be2247996222d67c8c33038
Gürkan <gurkan.andac@hva.nl>
2025-10-03 18:19:46 +0200

    depends on

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,16 +5,20 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_storage_account" "storage" {
   name                     = var.storage_account_name
-  resource_group_name      = var.resource_group_name
-  location                 = var.resource_group_location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+  depends_on = [azurerm_resource_group.rg]
 }
 
 resource "azurerm_service_plan" "func_plan" {
   name                = "weather-func-plan"
-  resource_group_name = var.resource_group_name
-  location            = var.resource_group_location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
   sku_name            = "Y1"
+
+  depends_on = [azurerm_resource_group.rg]
 }
